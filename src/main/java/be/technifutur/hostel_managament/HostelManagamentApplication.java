403372b15c55b8461ffd260a11c20fe6df3e0838
sbstnlambert <sbstn.lambert@gmail.com>
2022-03-31 16:47:51 +0200

    Project initialization

---
+++ src/main/java/be/technifutur/hostel_managament/HostelManagamentApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.hostel_managament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelManagamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelManagamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technifutur/hostel_managament/HostelManagamentApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.hostel_managament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelManagamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
