7ccf3ee72ce6cd241e48aea11e8c035434577e35
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-01 11:06:50 +0200

    Initialize repositories

--- src/main/java/be/technifutur/hotel_managament/HotelManagamentApplication.java
+++ src/main/java/be/technifutur/hotel_managament/HotelManagamentApplication.java
@@ -1,13 +1,25 @@
 package be.technifutur.hotel_managament;
 
+import be.technifutur.hotel_managament.repositories.ClientRepository;
+import be.technifutur.hotel_managament.repositories.HotelRepository;
+import be.technifutur.hotel_managament.repositories.ManagerRepository;
+import be.technifutur.hotel_managament.repositories.RoomRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HotelManagamentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelManagamentApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(HotelManagamentApplication.class, args);
+
+		ClientRepository clientRepo = context.getBean(ClientRepository.class);
+		HotelRepository hotelRepo = context.getBean(HotelRepository.class);
+		ManagerRepository managerRepo = context.getBean(ManagerRepository.class);
+		RoomRepository roomRepo = context.getBean(RoomRepository.class);
+
 	}
 
 }
