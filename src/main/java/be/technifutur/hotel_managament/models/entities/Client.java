f9d7581a894940ab1198dc04036d9120d17a54a6
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-01 11:03:49 +0200

    Initialize ArrayList for rooms

--- src/main/java/be/technifutur/hotel_managament/models/entities/Client.java
+++ src/main/java/be/technifutur/hotel_managament/models/entities/Client.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -26,6 +27,6 @@ public class Client {
     @Column(nullable = false, name = "client_surname")
     private String surname;
 
-    @ManyToMany(mappedBy = "client")
-    private List<Room> rooms;
+    @ManyToMany(mappedBy = "clients")
+    private List<Room> rooms = new ArrayList<>();
 }
