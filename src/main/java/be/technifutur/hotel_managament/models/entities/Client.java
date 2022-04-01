6288873472a0bf586c40282851fd4b0c557c25b7
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-01 09:25:02 +0200

    Add Client Entity

---
+++ src/main/java/be/technifutur/hotel_managament/models/entities/Client.java
@@ -0,0 +1,27 @@
+package be.technifutur.hotel_managament.models.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, name = "client_name")
+    private String name;
+
+    @Column(nullable = false, name = "client_surname")
+    private String surname;
+}
