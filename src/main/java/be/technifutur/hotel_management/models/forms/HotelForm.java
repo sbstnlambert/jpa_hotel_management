366cd7d9fdc506b119cc911e0217b5d5d4c61cdc
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-04 16:31:22 +0200

    Implement HotelForm

---
+++ src/main/java/be/technifutur/hotel_management/models/forms/HotelForm.java
@@ -0,0 +1,12 @@
+package be.technifutur.hotel_management.models.forms;
+
+import lombok.Data;
+
+@Data
+public class HotelForm {
+
+    private byte starNumber;
+    private String name;
+    private String address;
+
+}
