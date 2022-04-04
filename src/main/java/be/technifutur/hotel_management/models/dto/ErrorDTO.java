dc9e5425a5b5d0bbc44a30d3763baac3b0b30158
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-04 14:00:08 +0200

    Add HttpMethod class field

--- src/main/java/be/technifutur/hotel_management/models/dto/ErrorDTO.java
+++ src/main/java/be/technifutur/hotel_management/models/dto/ErrorDTO.java
@@ -2,6 +2,7 @@ package be.technifutur.hotel_management.models.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpMethod;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,24 +14,28 @@ public class ErrorDTO {
     private final String message;
     private final int status;
     private final String uri;
+    private final HttpMethod method;
     private final Map<String, Object> info = new HashMap<>();
 
-    public ErrorDTO(String message, String uri) {
+    public ErrorDTO(String message, String uri, HttpMethod method) {
         this.message = message;
+        this.method = method;
         this.status = 400;
         this.uri = uri;
     }
 
-    public ErrorDTO(String message, int status, String uri) {
+    public ErrorDTO(String message, int status, String uri, HttpMethod method) {
         this.message = message;
         this.status = status;
         this.uri = uri;
+        this.method = method;
     }
 
-    public ErrorDTO(String message, int status, String uri, Map<String, Object> info) {
+    public ErrorDTO(String message, int status, String uri, Map<String, Object> info, HttpMethod method) {
         this.message = message;
         this.status = status;
         this.uri = uri;
+        this.method = method;
         this.info.putAll(info);
     }
 }
