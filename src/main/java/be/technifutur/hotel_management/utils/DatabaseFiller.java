94b46c919a9851a8fb33e990a463d2c38e865ca4
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-04 16:56:33 +0200

    Create new managers & hotel for test

--- src/main/java/be/technifutur/hotel_management/utils/DatabaseFiller.java
+++ src/main/java/be/technifutur/hotel_management/utils/DatabaseFiller.java
@@ -1,24 +1,49 @@
 package be.technifutur.hotel_management.utils;
 
 import be.technifutur.hotel_management.models.entities.Hotel;
+import be.technifutur.hotel_management.models.entities.Manager;
 import be.technifutur.hotel_management.repositories.HotelRepository;
+import be.technifutur.hotel_management.repositories.ManagerRepository;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
+@Component
 public class DatabaseFiller implements InitializingBean {
 
-    private final HotelRepository repository;
+    private final HotelRepository hotelRepository;
+    private final ManagerRepository managerRepository;
 
-    public DatabaseFiller (HotelRepository repository) {
-        this.repository = repository;
+    public DatabaseFiller (HotelRepository hotelRepository, ManagerRepository managerRepository) {
+        this.hotelRepository = hotelRepository;
+        this.managerRepository = managerRepository;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
 
-        Hotel hotel = new Hotel();
-        hotel.setName("Hilton");
-        hotel.setStarNumber((byte)4);
+        Manager l = Manager.builder()
+                .beginCareerOn(LocalDate.now())
+                .surname("Lionel")
+                .name("Delsupexhe")
+                .build();
+        managerRepository.save(l);
 
-        repository.save(hotel);
+        Manager e = Manager.builder()
+                .beginCareerOn(LocalDate.now().minusDays(2))
+                .surname("Estelle")
+                .name("Huynen")
+                .build();
+        managerRepository.save(e);
+
+        Hotel hotel = Hotel.builder()
+                .starNumber((byte)4)
+                .name("Hilton")
+                .address("8, Bervely Lane, California")
+                .owner(e)
+                .build();
+
+        hotelRepository.save(hotel);
     }
 }
