8a0ad4d3166d819cf297ce7cdf4e25ac0601fa85
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-04 11:52:16 +0200

    Create custom errors

---
+++ src/main/java/be/technifutur/hotel_management/models/dto/ErrorDTO.java
@@ -0,0 +1,34 @@
+package be.technifutur.hotel_management.models.dto;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class ErrorDTO {
+
+    private final String message;
+    private final int status;
+    private final String uri;
+    private final Map<String, Object> info = new HashMap<>();
+
+    public ErrorDTO(String message, String uri) {
+        this.message = message;
+        this.status = 400;
+        this.uri = uri;
+    }
+
+    public ErrorDTO(String message, int status, String uri) {
+        this.message = message;
+        this.status = status;
+        this.uri = uri;
+    }
+
+    public ErrorDTO(String message, int status, String uri, Map<String, Object> info) {
+        this.message = message;
+        this.status = status;
+        this.uri = uri;
+        this.info.putAll(info);
+    }
+}
