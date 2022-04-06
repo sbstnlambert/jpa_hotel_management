071cac03a53ba57cbd5c74ed4f6e213810488e9b
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-06 11:48:02 +0200

    Add Manager class property

--- src/main/java/be/technifutur/hotel_management/models/forms/HotelForm.java
+++ src/main/java/be/technifutur/hotel_management/models/forms/HotelForm.java
@@ -1,5 +1,6 @@
 package be.technifutur.hotel_management.models.forms;
 
+import be.technifutur.hotel_management.models.entities.Manager;
 import lombok.Data;
 
 @Data
@@ -8,5 +9,6 @@ public class HotelForm {
     private byte starNumber;
     private String name;
     private String address;
+    private Manager manager;
 
 }
