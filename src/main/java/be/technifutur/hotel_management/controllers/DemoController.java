400b85cfa78559fd03157fe23ce45214010efc3a
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-06 16:57:54 +0200

    Implement Security Access annotations

--- src/main/java/be/technifutur/hotel_management/controllers/DemoController.java
+++ src/main/java/be/technifutur/hotel_management/controllers/DemoController.java
@@ -1,9 +1,15 @@
 package be.technifutur.hotel_management.controllers;
 
+import org.springframework.context.annotation.Role;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/demo")
@@ -42,23 +48,29 @@ public class DemoController {
     }
 
     // SECURITE
-
+    // Les rôles donnent accès aux authorities, pas l'inverse
     @GetMapping("/for-all")
+    @PreAuthorize("permitAll()")
     public String getForAll() {
         return "For all";
     }
 
     @GetMapping("/for-connected")
+    @PreAuthorize("isAuthenticated()")
     public String getForConnected() {
         return "For connected";
     }
 
     @GetMapping("/for-user")
+//    @Secured("ROLE_USER")
+    @RolesAllowed("ROLE_USER")
     public String getForUser() {
         return "For user";
     }
 
     @GetMapping("/for-admin")
+    @Secured("ROLE_ADMIN")
+//    @RolesAllowed("ROLE_ADMIN")
     public String getForAdmin() {
         return "For admin";
     }
