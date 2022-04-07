e746676f05c1895fa70ec9f3c4d4d33fabbcdf96
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-07 09:41:01 +0200

    Add userDetailsService property

--- src/main/java/be/technifutur/hotel_management/config/WebSecurityConfig.java
+++ src/main/java/be/technifutur/hotel_management/config/WebSecurityConfig.java
@@ -1,13 +1,13 @@
 package be.technifutur.hotel_management.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 // WebSecurityConfigurerAdapter permet de redéfinir les méthodes qui définissent la sécurité de mon app
@@ -24,29 +24,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // Après avoir créé PasswordEncryptionConfig
     private final PasswordEncoder encoder;
+    private final UserDetailsService userDetailsService;
 
-    public WebSecurityConfig(PasswordEncoder encoder) {
+    public WebSecurityConfig(PasswordEncoder encoder, UserDetailsService userDetailsService) {
         this.encoder = encoder;
+        this.userDetailsService = userDetailsService;
     }
 
     // AuthenticationManagerBuilder crée l'Objet qui va définir si je suis connecté ou non, et avec quel rôle (authorities)
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder);
+
         // Forme la plus simple d'authentification : en mémoire (pas bien du tout)
         // Données supprimées quand l'app est fermée
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(encoder.encode("userpassword"))
-                // Les rôles donnent accès aux authorities, pas l'inverse
-//                .authorities("USER")
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password(encoder.encode("adminpassword"))
-//                .authorities("USER", "ADMIN")
-                .roles("USER", "ADMIN");
-                // => Pas bien car on doit prendre l'habitude de ne pas donner nos mdp en dur, au clair
-                // GOTO create PasswordEncryptionConfig
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password(encoder.encode("userpassword"))
+//                // Les rôles donnent accès aux authorities, pas l'inverse
+////                .authorities("USER")
+//                .roles("USER")
+//                .and()
+//                .withUser("admin")
+//                .password(encoder.encode("adminpassword"))
+////                .authorities("USER", "ADMIN")
+//                .roles("USER", "ADMIN");
+//                // => Pas bien car on doit prendre l'habitude de ne pas donner nos mdp en dur, au clair
+//                // GOTO create PasswordEncryptionConfig
     }
 
     // Ensuite je veux sécuriser les méthodes grâce à HttpSecurity
