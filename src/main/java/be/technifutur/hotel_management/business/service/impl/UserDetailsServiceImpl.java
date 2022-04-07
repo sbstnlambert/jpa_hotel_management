0957eb552961f158f208c588327a0049f0eb4bfa
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-07 10:59:16 +0200

    Implement class method to findByUser

--- src/main/java/be/technifutur/hotel_management/business/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/be/technifutur/hotel_management/business/service/impl/UserDetailsServiceImpl.java
@@ -1,12 +1,24 @@
 package be.technifutur.hotel_management.business.service.impl;
 
+import be.technifutur.hotel_management.data.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    // Besoin du repository pour accéder aux entités de la DB
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        // Je veux trouver un user par sans username, sans faire de findAll() ni trouver par id
+        // Dans UserRepository, je crée une méthode findByUsername() grâce à JPA Data
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
     }
 }
