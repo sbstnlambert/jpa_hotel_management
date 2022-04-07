f4b179a950cd0e41c4a8325e99eeae42c90627af
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-07 09:14:49 +0200

    Create User and implements UserDetails interface methods

---
+++ src/main/java/be/technifutur/hotel_management/models/User.java
@@ -0,0 +1,45 @@
+package be.technifutur.hotel_management.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "userpassword";
+    }
+
+    @Override
+    public String getUsername() {
+        return "user";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
