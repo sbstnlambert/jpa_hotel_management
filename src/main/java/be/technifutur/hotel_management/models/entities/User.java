ef47bcc364796dcbd477f8ff0b397f27a8905486
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-07 10:09:44 +0200

    Create User Entity

---
+++ src/main/java/be/technifutur/hotel_management/models/entities/User.java
@@ -0,0 +1,66 @@
+package be.technifutur.hotel_management.models.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "DB_User")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean isNotLocked = true;
+
+    // Rappel : EAGER s'oppose à LAZY
+    // LAZY = Les rôles ne seront récupérés que lorsque je fais getRole()
+    // Ce que je ne veux pas
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles().stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isNotLocked;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isNotLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isNotLocked;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isNotLocked;
+    }
+}
