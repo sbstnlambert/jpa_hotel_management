07832ba9324b3eb9d8a036c99d1aafddef30bf09
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-07 09:15:24 +0200

    Create UserDetailsServiceImpl class

---
+++ src/main/java/be/technifutur/hotel_management/business/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,6 @@
+package be.technifutur.hotel_management.business.service.impl;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class UserDetailsServiceImpl {
+}
